fb93aaaabb1e55080bb709af2c407395fc014f2e
woojeong kong <coolstevevc@gmail.com>
2022-08-10 01:17:52 +0900

    feat: Add GameObject, Circle class

---
+++ src/game.ts
@@ -0,0 +1,50 @@
+class GameObject {
+  context: CanvasRenderingContext2D;
+  x: number;
+  y: number;
+  vx: number;
+  vy: number;
+  isColliding: boolean;
+
+  constructor(
+    context: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    vx: number,
+    vy: number
+  ) {
+    this.context = context;
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+
+    this.isColliding = false;
+  }
+}
+
+class Circle extends GameObject {
+  radius: number;
+
+  constructor(
+    context: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    vx: number,
+    vy: number,
+    radius: number
+  ) {
+    super(context, x, y, vx, vy);
+    this.radius = radius;
+  }
+
+  draw() {
+    this.context.beginPath();
+    this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    this.context.fillStyle = "black";
+    this.context.fill();
+    this.context.closePath();
+  }
+}
+
+export { Circle };
