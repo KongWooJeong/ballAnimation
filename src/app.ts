e8da0deea422c8e69e466ba54341860da79e5dcf
woojeong kong <coolstevevc@gmail.com>
2022-08-10 01:38:10 +0900

    feat: Add gameLoop method

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-// init
+import { Circle } from "./game";
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+const ball = new Circle(ctx, 40, 60, 10, 10, 10);
+
+function gameLoop() {
+  ball.update();
+  ball.clear();
+  ball.draw();
+
+  window.requestAnimationFrame(gameLoop);
+}
+
+window.requestAnimationFrame(gameLoop);
