8e4754697d0b0cef87ac05d2d2cfc32255ef82e9
woojeong kong <coolstevevc@gmail.com>
2022-08-11 01:38:03 +0900

    feat: Add GameWorld class

--- src/game.ts
+++ src/game.ts
@@ -1,25 +1,25 @@
 class GameObject {
   context: CanvasRenderingContext2D;
+  angle: number;
   x: number;
   y: number;
   vx: number;
   vy: number;
-  isColliding: boolean;
 
   constructor(
     context: CanvasRenderingContext2D,
     x: number,
     y: number,
     vx: number,
-    vy: number
+    vy: number,
+    angle: number
   ) {
     this.context = context;
     this.x = x;
     this.y = y;
     this.vx = vx;
     this.vy = vy;
-
-    this.isColliding = false;
+    this.angle = angle;
   }
 }
 
@@ -32,16 +32,17 @@ class Circle extends GameObject {
     y: number,
     vx: number,
     vy: number,
-    radius: number
+    radius: number,
+    angle: number
   ) {
-    super(context, x, y, vx, vy);
+    super(context, x, y, vx, vy, angle);
     this.radius = radius;
   }
 
   draw() {
     this.context.beginPath();
     this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    this.context.fillStyle = this.isColliding ? "#ff8080" : "#0099b0";
+    this.context.fillStyle = "#0099b0";
     this.context.fill();
     this.context.closePath();
   }
@@ -56,12 +57,33 @@ class Circle extends GameObject {
   }
 }
 
+class GameWorld {
+  canvas: HTMLCanvasElement | null;
+  context: CanvasRenderingContext2D | null;
+  secondsPassed: number;
+  oldTimeStamp: number;
+  gameEntities: [];
+
+  constructor(canvasId: string) {
+    this.canvas = null;
+    this.context = null;
+    this.secondsPassed = 0;
+    this.oldTimeStamp = 0;
+    this.gameEntities = [];
+
+    this.init(canvasId);
+  }
+
+  init(canvasId: string) {
+    this.canvas = document.getElementById(canvasId) as HTMLCanvasElement;
+    this.context = this.canvas.getContext("2d");
+  }
+}
+
 function circleIntersect(x1: number, y1: number, x2: number, y2: number) {
   const squareDistance = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
-
-  console.log(squareDistance);
 
   return squareDistance;
 }
 
-export { Circle, circleIntersect };
+export { Circle, GameWorld, circleIntersect };
